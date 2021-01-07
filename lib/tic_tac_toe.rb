6b77949769ec8b7ea04b329308a7b4245a1bdf5d
Robert Stine <rcorystine@gmail.com>
2021-01-07 17:32:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,7 @@ class TicTacToe
   def turn_count
     count = 0
     @board.each do |spaces|
-      if spaces == "X" || spaces == "0"
+      if spaces == "X" || spaces == "O"
       count += 1 
       end
     end
