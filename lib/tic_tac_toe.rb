b46d65cc2e1001f41ebe1067bf3617b07607405a
Robert Stine <rcorystine@gmail.com>
2021-01-07 18:08:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -81,7 +81,8 @@ class TicTacToe
     WIN_COMBINATIONS.detect do |each|
       @board[combo[0]] == @board[combo[1]] &&
       @board[combo[1]] == @board[combo[2]] &&
-      position_taken?(board, combo[0])
+      position_taken?(combo[0])
+    end
   end
   
   def full?
